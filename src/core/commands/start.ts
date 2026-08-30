5b2a6f48e2e38948c6657c1a6e34b3af1be43eea
quarnel-dev <quarnel.dev@gmail.com>
2026-08-30 16:55:12 +0300

    feat: use i18n in start command

--- src/core/commands/start.ts
+++ src/core/commands/start.ts
@@ -1,5 +1,7 @@
 import type { Context } from 'grammy'
 
+import { t } from '#locales/index.ts'
+
 export function startCommand(ctx: Context) {
-  return ctx.reply('Hello World')
+  return ctx.reply(t('start.welcome'))
 }
