7c6c7395e2057e334946fd2624fc1327a7801d88
quarnel-dev <quarnel.dev@gmail.com>
2026-09-12 16:44:50 +0300

    refactor(bot): use logger in error handler

--- src/core/bot.core.ts
+++ src/core/bot.core.ts
@@ -1,4 +1,5 @@
 import { Bot } from 'grammy'
+
 import { startCommand } from './screens/start/start.command.ts'
 import { menuCommand } from './screens/menu/menu.command.ts'
 import { handleMenuCallback } from './handlers/menu.handler.ts'
@@ -6,12 +7,18 @@ import { handleSteamCallback } from './handlers/steam.handler.ts'
 import { handleSettingsCallback } from './handlers/settings.handler.ts'
 import { ensureUser } from './middlewares/ensureUser.middleware.ts'
 
+import { logger } from '#logger/index.ts'
+
 export function createBot(token: string) {
   const bot = new Bot(token)
 
   bot.catch((err) => {
     const ctx = err.ctx
-    console.error(`Error while handling update ${ctx.update.update_id}: `, err.error)
+    logger.error('unhandled error while processing update', {
+      module: 'bot',
+      updateId: ctx.update.update_id,
+      err: err.error,
+    })
   })
 
   bot.use(ensureUser)
@@ -22,6 +29,8 @@ export function createBot(token: string) {
   bot.callbackQuery(/^menu_/, handleMenuCallback)
   bot.callbackQuery(/^steam_/, handleSteamCallback)
   bot.callbackQuery(/^settings_/, handleSettingsCallback)
+
+  logger.debug('bot created with commands and callbacks registered', { module: 'bot' })
 
   return bot
 }
